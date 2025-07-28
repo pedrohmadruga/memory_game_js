b9c72d819f01a9afbe8d3f9afd3747b865df44db
Pedro Madruga <82350191+pedrohmadruga@users.noreply.github.com>
2025-07-28 10:29:33 -0300

    Update script.js -> Only allow two cards to be up

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
+"use strict";
+
 const cards = document.querySelectorAll(".card");
 const newGameBtn = document.querySelector(".new_game_btn");
+const overlay = document.querySelector(".freeze-overlay");
 
-let isShuffling = false;
+// Global variables
+let isShuffling, cardsFlippedArray;
+
+function startNewGame() {
+    isShuffling = false;
+    currentCardsFlipped = 0;
+    totalCardsFlipped = 0;
+    cardsFlippedArray = [];
+    shuffleCards();
+}
+
+function shuffleCards() {
+    return null;
+}
+
+startNewGame();
 
 cards.forEach(card => {
     card.addEventListener("click", () => {
-        if (isShuffling) return; // Ignores click if cards are shuffling
+        if (isShuffling || card.classList.contains("card_flipped")) return; // Ignores click if cards are shuffling or if already flipped
         card.classList.toggle("card_flipped");
+
+        if (card.classList.contains("card_flipped")) {
+            currentCardsFlipped++;
+            cardsFlippedArray.push(card);
+            if (currentCardsFlipped == 2) {
+                // stop all click events for 1 second
+                overlay.classList.remove("hidden");
+
+                setTimeout(() => {
+                    overlay.classList.add("hidden");
+                    cardsFlippedArray.forEach(card =>
+                        card.classList.remove("card_flipped")
+                    );
+                    currentCardsFlipped = 0;
+                    cardsFlippedArray = [];
+                }, 1500);
+            }
+        }
     });
 });
 
@@ -17,7 +53,8 @@ newGameBtn.addEventListener("click", () => {
         card.classList.remove("card_flipped");
     });
 
-    // Rotates card
+    // TODO: Refactor code, inner is currently being repeated in the three timeouts
+    // Step 1: Rotates card
     setTimeout(() => {
         cards.forEach(card => {
             const inner = card.querySelector(".card_inner");
@@ -26,7 +63,7 @@ newGameBtn.addEventListener("click", () => {
         });
     }, 600); // flip card animation time
 
-    // Fase 2: Shuffles card
+    // Step 2: Shuffles card
     setTimeout(() => {
         cards.forEach(card => {
             const inner = card.querySelector(".card_inner");
@@ -35,7 +72,7 @@ newGameBtn.addEventListener("click", () => {
         });
     }, 1100); // rotate animation time + flip card time
 
-    // Fase 3: Returns to original state
+    // Step 3: Returns to original state
     setTimeout(() => {
         cards.forEach(card => {
             const inner = card.querySelector(".card_inner");
@@ -43,5 +80,9 @@ newGameBtn.addEventListener("click", () => {
             inner.classList.add("returning");
         });
         isShuffling = false;
+        startNewGame();
     }, 2100); // full time
 });
+
+// TODO: Game logic
+// TODO: Congratulations pop up that gets bigger
